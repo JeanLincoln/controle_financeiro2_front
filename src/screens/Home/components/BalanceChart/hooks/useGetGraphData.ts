685ab5e81fe1abc72ed0bc7e8e4764bd4bdb9701
Jean Lincoln <lincolnnDev@gmail.com>
2025-07-30 14:47:11 -0300

    CF2-010: refactor: convert fetchGraphData to useCallback for improved performance

--- src/screens/Home/components/BalanceChart/hooks/useGetGraphData.ts
+++ src/screens/Home/components/BalanceChart/hooks/useGetGraphData.ts
@@ -1,5 +1,6 @@
 import { useLazyTransactionsGraphQuery } from "@/store/services/dashboard/dashboard.service";
 import { handleRequest } from "@/utils/handleRequest";
+import { useCallback } from "react";
 import { toast } from "sonner";
 
 interface RangeDateAPIProps {
@@ -13,20 +14,23 @@ export const useGetGraphData = () => {
   const [getGraphData, { data: graphData, isLoading }] =
     useLazyTransactionsGraphQuery();
 
-  const fetchGraphData = async ({ rangeDate }: RangeDateAPIProps) => {
-    const startDate = rangeDate.from;
-    const endDate = rangeDate.to;
-    const [error] = await handleRequest(
-      getGraphData({ startDate, endDate }).unwrap()
-    );
-
-    if (error) {
-      toast.error(
-        "Houve um erro ao carregar os dados do gráfico. Tente novamente."
+  const fetchGraphData = useCallback(
+    async ({ rangeDate }: RangeDateAPIProps) => {
+      const startDate = rangeDate.from;
+      const endDate = rangeDate.to;
+      const [error] = await handleRequest(
+        getGraphData({ startDate, endDate }).unwrap()
       );
-      return;
-    }
-  };
+
+      if (error) {
+        toast.error(
+          "Houve um erro ao carregar os dados do gráfico. Tente novamente."
+        );
+        return;
+      }
+    },
+    [getGraphData]
+  );
 
   return {
     fetchGraphData,
