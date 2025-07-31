5b3e6b46fe57c8539aec0572736646ec1c394e32
Jean Lincoln <lincolnnDev@gmail.com>
2025-07-31 09:51:52 -0300

    CF2-013: feat: implement dynamic width and padding for Aside and DefaultLayout components

---
+++ src/components/Layouts/DefaultLayout/constants/asideExpansionSpacing.constant.ts
@@ -0,0 +1,14 @@
+const spacing = {
+  collapsed: 24,
+  expanded: 52
+};
+
+export const asideExpansionMainPadding = {
+  collapsed: `pl-${spacing.collapsed}`,
+  expanded: `pl-${spacing.expanded}`
+};
+
+export const asideExpansionAsideWidth = {
+  collapsed: `w-${spacing.collapsed}`,
+  expanded: `w-${spacing.expanded}`
+};
