638d3e85757cba6fa921f1868b1d47c21f14d4cf
Jean Lincoln <lincolnnDev@gmail.com>
2025-08-05 19:51:44 -0300

    CF2-014: Adjusting layout height

--- src/components/Layouts/DefaultLayout/DefaultLayout.component.tsx
+++ src/components/Layouts/DefaultLayout/DefaultLayout.component.tsx
@@ -5,7 +5,7 @@ function DefaultLayout() {
   return (
     <div className="bg-background min-h-screen">
       <Aside />
-      <main className="w-full min-h-full transition-all duration-300 ease-in-out pl-24 peer-hover:pl-54">
+      <main className="w-full min-h-screen transition-all duration-300 ease-in-out pl-24 peer-hover:pl-54">
         <Outlet />
       </main>
     </div>
