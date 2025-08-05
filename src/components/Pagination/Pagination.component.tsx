2e63f3d45d2c3a27250c4b29ea1681fa7ff60f48
Jean Lincoln <lincolnnDev@gmail.com>
2025-08-05 19:50:43 -0300

    CF2-014: Setting pagination

--- src/components/Pagination/Pagination.component.tsx
+++ src/components/Pagination/Pagination.component.tsx
@@ -7,6 +7,9 @@ import * as React from "react";
 
 import { Button, buttonVariants } from "@/components/Button/Button.component";
 import { cn } from "@/utils/cn.utils";
+import type { PaginationResponse } from "@/store/services/services.types";
+import { usePagination } from "./hooks/usePagination";
+import { Input } from "../Input/Input.component";
 
 function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
   return (
@@ -77,7 +80,7 @@ function PaginationPrevious({
       {...props}
     >
       <ChevronLeftIcon />
-      <span className="hidden sm:block">Previous</span>
+      <span className="hidden sm:block">Anterior</span>
     </PaginationLink>
   );
 }
@@ -93,7 +96,7 @@ function PaginationNext({
       className={cn("gap-1 px-2.5 sm:pr-2.5", className)}
       {...props}
     >
-      <span className="hidden sm:block">Next</span>
+      <span className="hidden sm:block">Próximo</span>
       <ChevronRightIcon />
     </PaginationLink>
   );
@@ -116,6 +119,187 @@ function PaginationEllipsis({
   );
 }
 
+type StandardPaginationProps = {
+  onChangePage: (page: number) => void;
+  className?: string;
+  paginationProps?: PaginationResponse["pagination"];
+};
+
+function StandardPagination({
+  className,
+  paginationProps,
+  onChangePage
+}: StandardPaginationProps) {
+  if (!paginationProps) return null;
+
+  const { page, firstPage, lastPage, hasPrevious, hasNext, totalPages } =
+    paginationProps;
+
+  const {
+    farFromStart,
+    farFromEnd,
+    allItemsArray,
+    arrayFarFromStart,
+    arrayFarFromEnd,
+    handleChangePage,
+    handleGoBack,
+    handleGoForward,
+    handleSelectPageInput
+  } = usePagination({ page, onChangePage, totalPages });
+
+  const renderPageItems = () => {
+    if (!farFromStart && farFromEnd)
+      return (
+        <>
+          {arrayFarFromStart.map((renderPage) => (
+            <PaginationItem key={renderPage}>
+              <PaginationLink
+                isActive={renderPage === page}
+                onClick={() => handleChangePage(renderPage)}
+              >
+                {renderPage}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              isActive={page === lastPage}
+              onClick={() => handleChangePage(lastPage)}
+            >
+              {lastPage}
+            </PaginationLink>
+          </PaginationItem>
+        </>
+      );
+
+    if (farFromStart && !farFromEnd)
+      return (
+        <>
+          <PaginationItem>
+            <PaginationLink
+              isActive={page === firstPage}
+              onClick={() => handleChangePage(firstPage)}
+            >
+              {firstPage}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          {arrayFarFromEnd.map((renderPage) => (
+            <PaginationItem key={renderPage}>
+              <PaginationLink
+                isActive={renderPage === page}
+                onClick={() => handleChangePage(renderPage)}
+              >
+                {renderPage}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+        </>
+      );
+
+    if (farFromStart && farFromEnd) {
+      return (
+        <>
+          <PaginationItem>
+            <PaginationLink
+              isActive={false}
+              onClick={() => handleChangePage(firstPage)}
+            >
+              {firstPage}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              isActive={false}
+              onClick={() => handleChangePage(page - 1)}
+            >
+              {page - 1}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              isActive={true}
+              onClick={() => handleChangePage(page)}
+            >
+              {firstPage}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              isActive={false}
+              onClick={() => handleChangePage(page + 1)}
+            >
+              {page + 1}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              isActive={page === lastPage}
+              onClick={() => handleChangePage(lastPage)}
+            >
+              {lastPage}
+            </PaginationLink>
+          </PaginationItem>
+        </>
+      );
+    }
+
+    return allItemsArray.map((renderedPage) => (
+      <PaginationItem key={renderedPage}>
+        <PaginationLink
+          isActive={renderedPage === page}
+          onClick={() => handleChangePage(renderedPage)}
+        >
+          {renderedPage}
+        </PaginationLink>
+      </PaginationItem>
+    ));
+  };
+
+  return (
+    <Pagination className={cn("h-fit flex gap-2", className)}>
+      <PaginationContent>
+        {hasPrevious && (
+          <>
+            <PaginationItem>
+              <PaginationPrevious onClick={handleGoBack} />
+            </PaginationItem>
+          </>
+        )}
+        {renderPageItems()}
+        {hasNext && (
+          <>
+            <PaginationItem>
+              <PaginationNext href="#" onClick={handleGoForward} />
+            </PaginationItem>
+          </>
+        )}
+      </PaginationContent>
+      <div className="flex items-center justify-center gap-2">
+        <Input
+          className=" [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [-moz-appearance:textfield]"
+          type="number"
+          min={1}
+          max={totalPages}
+          onChange={handleSelectPageInput}
+        />
+        <span>/ {totalPages}</span>
+      </div>
+    </Pagination>
+  );
+}
+
 export {
   Pagination,
   PaginationContent,
@@ -123,5 +307,6 @@ export {
   PaginationItem,
   PaginationLink,
   PaginationNext,
-  PaginationPrevious
+  PaginationPrevious,
+  StandardPagination
 };
